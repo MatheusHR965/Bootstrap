cd023f2cdae8907fea8f68d396704130767bd6e9
matheus <matheus835Hq@gmail.com>
2024-08-05 14:34:59 -0300

    bootstrap

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+
+$localhost = "localhost";
+$username = "root";
+$password = "";
+$dbname = "samueldb";
+
+$con = mysqli_connect($localhost,$username, $password, $dbname);
+    if($con->connect_error) {   
+        die("connect failed: " .$con->connect_error);}
